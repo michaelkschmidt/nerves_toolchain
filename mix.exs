dbaa996f811a97b0c90abed250802bf54534cbf6
Justin Schneck <jschneck@mac.com>
2016-05-08 16:52:38 -0400

    updated hex package information

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule NervesToolchain.Mixfile do
     [app: :nerves_toolchain,
      version: "0.6.1",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -13,8 +15,18 @@ defmodule NervesToolchain.Mixfile do
   end
 
   defp deps do
-    [
-      {:httpoison, "~> 0.8"}
-    ]
+    [{:httpoison, "~> 0.8"}]
+  end
+
+  defp description do
+    """
+    Elixir compilers and scripts for building Nerves Toolchains. For useable toolchain configurations see nerves_toolchain_*
+    """
+  end
+
+  defp package do
+    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/nerves-project/nerves_toolchain"}]
   end
 end
